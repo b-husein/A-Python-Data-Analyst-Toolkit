4601c0556b69330d92a4f052281ca020e9c55067
b-husein <bajrektarevic.husein@gmail.com>
2021-01-01 08:57:11 +0100

    updated

--- 02_python_introduction/02_python_interpreter.py
+++ 02_python_introduction/02_python_interpreter.py
@@ -4,7 +4,12 @@
 # download link: https://www.python.org/downloads/
 
 """ 
-- 
+- When we go to download Python from the link above, we are actually downlading 'cpython';
 
+- Because Python has been written in C programming language.
+
+- It's a program written in C to read your Python file and run it.
+
+- There are other implementation like Jython, pypy, IronPython (written for .NET framework);
 
 """
---
+++ 02_python_introduction/03_run_python_code.py
@@ -0,0 +1,18 @@
+
+# DOWNLOAD PYTHON TRANSLATION SERVICE FROM OFFICIAL WEBSITE; 
+
+""" 
+- To write Python code we can use: 
+
+    - Terminal
+    - Code editors (like VScode or Sublime)
+    - IDEs (like PyCharm, Spyder)
+    - Notebooks (like Jupiter Notebooks)
+
+- Before we install any of these editors, we can actually first write some code in Python
+
+- using repl.it or glot.io; and using this we can save Python files online.
+
+- we don't have to download translation service but we can write Python code. 
+
+"""
