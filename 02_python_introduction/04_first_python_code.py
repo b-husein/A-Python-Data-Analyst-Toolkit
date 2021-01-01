a6b903e7b105b08fd961498b73f3746f92d234fd
b-husein <bajrektarevic.husein@gmail.com>
2021-01-01 09:35:46 +0100

    updated

---
+++ 02_python_introduction/04_first_python_code.py
@@ -0,0 +1,24 @@
+
+# first thing - to print simple function in Python;
+# another hint .py extension with python files.
+# in order to see the output or result, we need to run 
+# a command prompt (in VScode or Git Bash) 
+# and we can do that by typing the following line of code in terminal:
+# py name_of_the_file
+# and we will see the output in terminal.
+# ctrl + K to clear terminal
+
+# print('Hi there!')
+
+# let's make it little bit more dynamic;
+# using input function to capture some information.
+
+# input('Please write your name: ')
+
+# this is where code becomes interesting because we can store
+# this information on our computer.
+# let's assign the value of input into memory.
+
+name = input('What is your name? ')
+
+print('Hello ' + name)
