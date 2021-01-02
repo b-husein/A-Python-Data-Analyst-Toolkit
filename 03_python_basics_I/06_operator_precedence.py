e9bde8103a4f06211fe4fc625e3bc161925f7b77
b-husein <bajrektarevic.husein@gmail.com>
2021-01-02 08:43:14 +0100

    updated

---
+++ 03_python_basics_I/06_operator_precedence.py
@@ -0,0 +1,10 @@
+
+# very important term in programming
+# it means that different operators have presedence over different ones;
+
+print(20 - 3 * 4) # result is 8
+
+# () - first we solve parenthesis
+# ** - then we solve power of
+# * and /
+# + and -
