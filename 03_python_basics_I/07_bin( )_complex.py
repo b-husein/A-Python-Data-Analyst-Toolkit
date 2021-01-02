4ad89d77dabc89e91d887bdc03dbeeed1281ed6d
b-husein <bajrektarevic.husein@gmail.com>
2021-01-02 09:03:01 +0100

    updated

---
+++ 03_python_basics_I/07_bin( )_complex.py
@@ -0,0 +1,10 @@
+
+# an extra data type called 'complex'; 
+# a complex number is third type; first is int, second is float.
+# we use it if we do complex math.
+
+# numbers get stored in memory as binary and there is bin() action that we can use;
+# for example for number 5;
+
+print(bin(5)) # output is 0b101
+
