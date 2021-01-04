c791e5fe17c76a733db48e2ee76179c27199547e
b-husein <bajrektarevic.husein@gmail.com>
2021-01-04 11:25:59 +0100

    updated

---
+++ 05_python_basics_II/01_input.py
@@ -0,0 +1,16 @@
+"""
+- The input function accepts inputs from the user. The input provided by the user is stored
+- as a variable of type String. 
+- If you want to do any mathematical calculations with any numeric input
+- you need to change the data type of the input to integer or float as follows:
+
+"""
+
+#CODE
+
+age = input("Enter your age:")
+
+print("In 2010, you were ", int(age)-10, "years old.")
+
+#OUTPUT: In 2010, you were 19 years old.
+
