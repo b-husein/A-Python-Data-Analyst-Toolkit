2f7df8c9871b71ae59f797395de37ac927416b7a
b-husein <bajrektarevic.husein@gmail.com>
2021-01-04 10:44:11 +0100

    updated

---
+++ 04_anaconda_inst/01_installation.py
@@ -0,0 +1,8 @@
+""" 
+- Anaconda is an open source platform used widely by Python programmers and data scientists; 
+- Installing this platform installs Python, the Jupyter notebook app; and additional libraries. 
+- The following are the steps for installation: 
+
+- URL: 
+
+"""
