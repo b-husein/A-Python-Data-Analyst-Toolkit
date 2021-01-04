c217e036b8dd1912e2e82a570864bfe85374ffad
b-husein <bajrektarevic.husein@gmail.com>
2021-01-04 11:32:40 +0100

    updated

---
+++ 05_python_basics_II/02_var_const.py
@@ -0,0 +1,21 @@
+ 
+# About variables and constants
+
+"""
+- A constant or a literal is a value that doesn't change, while 
+- a variable contains value that can be changed.
+- We define variable by giving it a name and assigning it a value.
+- Based on a value, a data type is automatically assigned to it.
+- Values are stored in variables using the assignment operator ( = ).
+
+- The rules of naming a variable in Python are as follows:
+    - a variable name cannot have spaces;
+    - a variable cannot start with a number;
+    - a variable name can contain only letters, numbers and underscore sign (_);
+    - a variable cannot take the name of reserved keyword (class, continue, break etc.)
+
+"""
+
+name = "Jack"
+age = 35
+married = False
