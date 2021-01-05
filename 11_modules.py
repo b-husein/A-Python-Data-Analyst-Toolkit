e970d30b8b8546382a009536e9526425effac7c2
b-husein <bajrektarevic.husein@gmail.com>
2021-01-05 17:52:57 +0100

    updated

---
+++ 11_modules.py
@@ -0,0 +1,34 @@
+
+#MODULES IN PYTHON
+
+''' 
+- A module is a Python file with a .py extension. It can be thought of as a section of a physical library.
+- Module contains sections that are related to one another.
+- For example, the matplotlib module contains all functions related to plotting graphs. 
+- A module can contain another module. The matplotlib module, for instance, contains a module called pyplot.
+
+- A module can be imported using the import keyword, followed by the name of the module:
+
+    # CODE:
+    import matplotlib
+
+- We can also import part of a module (a submodule or a function) using the from keyword. 
+- Example with the cosine function from the math module:
+
+    # CODE:
+    from math import cos
+
+- Creating and importing a customized module in Python requires the following steps:
+
+    1. Type 'idle' in the search bar next to the start menu. Once the Python shell
+    is open, create a new file by selecting: File -> New file.
+
+    2. Create some functions with similar functionality. As an example, here, we are creating
+    a simple module that creates two functions - sin_angle and cos_angle. These functions
+    calculate the sin and cosine of an angle (given in degrees).
+
+    # CODE: 
+
+
+
+'''
