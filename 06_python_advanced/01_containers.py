8e43465093eee308f271e9b586375f6995083561
b-husein <bajrektarevic.husein@gmail.com>
2021-01-06 08:45:49 +0100

    updated

--- 06_python_advanced/01_containers.py
+++ 06_python_advanced/01_containers.py
@@ -106,8 +106,81 @@
 
     - SORTING A LIST
     - The sort method sorts the values in the list, in ascending or descending order. 
-    
+    - By default, this method sorts the item in the ascending order. If the list contains strings, the sorting 
+    - is done in alphabetical order (using the first letter of each string), as shown in the following: 
 
+            #CODE
+            colors.sort()
+            colors
+
+            #OUTPUT:
+            ['blue', 'green', 'purple', 'red', 'violet', 'white', 'yellow']
+
+    - Note that the list must be homogeneous (all values in the list should be of the same data type) for the sort
+    - method to work. If the list contains items of different data types, applying the sort method leads to an error.
+
+    - If you want to sort your elements in the reverse alphabetical order, you need to add the reverse parameter
+    - and set it to 'True'
+
+        #CODE
+        colors.sort(reverse=True)
+        colors
+
+        OUTPUT:
+        ['yellow', 'white', 'violet', 'red', 'purple', 'green', 'blue']
+
+    - Note if you want to just reverse the order of the items in a list, without sorting the items 
+    - you can use the reverse method
+
+        #CODE
+        colors=['violet', 'indigo', 'red', 'blue', 'green', 'yellow']
+        colors.reverse()
+        colors
+
+        #OUTPUT
+        ['yellow', 'green', 'blue', 'red', 'indigo', 'violet',] 
+
+    
+    - SLICING A LIST
+    - When we create a slice from a list, we create a subset of the original list by choosing specific values
+    - from the list, based on their position or by using a condition. Slicing of a list works similar to 
+    - slicing a string, which we studied previously.
+
+    - To create a slice using an index, we use the colon operator ( : ) and specify the start and stop values
+    - for the indexed that need to be selected.
+
+    - If we provide no start or stop value before and after the colon, it is assumed that the start value
+    - is the index of the first element (0), and stop value is the index of the last element.
+
+        #CODE
+        colors[:]
+
+        #OUTPUT
+
+        ['Violet', 'Indigo', 'Blue', 'Green', 'Yellow', 'Orange', 'Red']
+
+    - We can also use the colon operator twice if we are using a step index. 
+    - In the following statement, alternate elements of the list are extracted by 
+    - specifying a step index of two.
+
+        #CODE
+        colors[::2]
+
+        #OUTPUT
+        ['Violet', 'Blue', 'Yellow', 'Red']
+        #every second one was sliced out
+
+    - Just like strings, lists follow both positive and negative indexing. 
+    - Negative indexing (starts from -1, which is te index of last element in the list) works from right to left
+    - while positive indexing (starts from 0, which is the index of the first element in the list) works from left to right
+
+    - an example of slicing with negative indexing, where we extract alternate elements starting from the last value in the list:
+
+        #CODE
+        colors[-1: -8: -2]
+
+        #OUTPUT
+        ['Red', 'Yellow', 'Blue', 'Violet']
 
 
 
