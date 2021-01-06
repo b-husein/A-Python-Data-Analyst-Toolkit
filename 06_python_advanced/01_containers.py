783f388b88cda50f2cd35f69b008cbfbebc415b0
b-husein <bajrektarevic.husein@gmail.com>
2021-01-06 07:48:21 +0100

    updated

---
+++ 06_python_advanced/01_containers.py
@@ -0,0 +1,25 @@
+
+# EXPLORING CONTAINERS, CLASSES and OBJECTS
+
+''' 
+- Previously we talked about variables that can have a data type like int, float and str;
+- but variable holds only a sinlge value. 
+- Containers are objects that can contain multiple values. These values can have the same or different data types.
+- The four built-in containers in Python are:
+
+    1. Lists
+    2. Tuples
+    3. Dictionaries
+    4. Sets
+
+- Containers are also called iterables; That is, you can visit or traverse through each of the values in a given container object.
+'''
+
+# 1. LISTS
+
+''' 
+- A list contains a set of values that are stored in sequential order. 
+- A list is mutable which means that we can modify, add or delete values in a list, making it a flexible container.
+
+
+'''
